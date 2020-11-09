f2b88bebaa17d9f30d3b55141810ca1a776117e3
ivanapavlovic314 <73428068+ivanapavlovic314@users.noreply.github.com>
2020-11-09 19:00:12 +0100

    Add files via upload

---
+++ firma.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `firma` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `firma`;
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: firma
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `zaposleni`
+--
+
+DROP TABLE IF EXISTS `zaposleni`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `zaposleni` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ime_i_prezime` varchar(60) NOT NULL,
+  `adresa` varchar(100) NOT NULL,
+  `starost` int NOT NULL,
+  `zarada` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zaposleni`
+--
+
+LOCK TABLES `zaposleni` WRITE;
+/*!40000 ALTER TABLE `zaposleni` DISABLE KEYS */;
+INSERT INTO `zaposleni` VALUES (5,'Nikola Pavlovic','Deset avijaticara 5',35,100000),(7,'Biljana Kecojevic','Marka Oreskovica 20',64,80000),(8,'Snezana Pavlovic','Kneza Lazara 40',50,50000),(9,'Ivana Pavlovic','Deset avijaticara 5',33,40000);
+/*!40000 ALTER TABLE `zaposleni` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-18 17:49:48
